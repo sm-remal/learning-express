55a0ef2f042b50751f4c9379738ef8a8ae8c7113
sm-remal <remal.mozzem@gmail.com>
2026-06-02 17:41:56 +0600

    Connection to PostgreSQL

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express, { type Application, type Request, type Response } from "express"
-const app: Application = express()
+import {Pool} from "pg"
+const app: Application = express() 
 const port = 5000
 
 // Middleware
 app.use(express.json());
+
+// Connection to PostgreSQL
+const pool = new Pool({
+    connectionString: "postgresql://neondb_owner:npg_qBbu6zIUWTS4@ep-young-dust-aq76qs2o.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require"
+})
 
 // Get request
 app.get('/', (req: Request, res: Response) => {
