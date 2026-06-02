d094e2f954d05d427b94a599d92b6b0f2b8d6904
sm-remal <remal.mozzem@gmail.com>
2026-06-02 16:51:14 +0600

    Update: server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
-import express from "express"
-const app = express()
+import express, { type Application, type Request, type Response } from "express"
+const app: Application = express()
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req: Request, res: Response) => {
+//   res.send('Hello World!')
+    res.status(200).json({
+        message: "Explore express with TypeScript",
+        author: "Next Level"
+    })
 })
 
 app.listen(port, () => {
