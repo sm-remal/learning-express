82986c23a5492b8875b148842ccc0bef7a8c7e87
sm-remal <remal.mozzem@gmail.com>
2026-06-02 17:10:03 +0600

    Use express middleware

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,21 @@ import express, { type Application, type Request, type Response } from "express"
 const app: Application = express()
 const port = 5000
 
+// Middleware
+app.use(express.json());
+
+// Get request
 app.get('/', (req: Request, res: Response) => {
 //   res.send('Hello World!')
     res.status(200).json({
         message: "Explore express with TypeScript",
         author: "Next Level"
     })
+})
+
+// post request
+app.post('/', async (req: Request, res: Response) => {
+    console.log(req.body)
 })
 
 app.listen(port, () => {
