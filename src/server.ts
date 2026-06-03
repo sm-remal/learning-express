f12d2bfdf3c4b4ac1d02ffc78f1262d3a84cbea1
sm-remal <remal.mozzem@gmail.com>
2026-06-03 12:28:00 +0600

    Create API: Get user by id

--- src/server.ts
+++ src/server.ts
@@ -48,8 +48,9 @@ app.get('/', (req: Request, res: Response) => {
     })
 })
 
+
 // post request
-app.post('/', async (req: Request, res: Response) => {
+app.post('/api/users', async (req: Request, res: Response) => {
     // console.log(req.body)
     const { name, email, password, age } = req.body;
 
@@ -62,12 +63,69 @@ app.post('/', async (req: Request, res: Response) => {
         // console.log(result.rows[0]);
 
         res.status(201).json({
+            success: true,
             message: "User created successfully",
             data: result.rows[0],
         })
     } catch (error: any) {
         res.status(500).json({
-            message: error.message, 
+            success: false,
+            message: error.message,
+            error: error,
+        })
+    }
+})
+
+
+
+// Get all users 
+app.get('/api/users', async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`
+            SELECT * FROM users
+            `)
+        res.status(200).json({
+            success: true,
+            message: "Users retrieved successfully",
+            data: result.rows,
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error retrieving users",
+            error: error,
+        })
+    }
+})
+
+
+
+// Get user by id
+app.get('/api/users/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const result = await pool.query(`
+            SELECT * FROM users WHERE id = $1 
+            `, [id]);
+
+        // If user not found
+        if (result.rows.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found",
+                data: {},
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "User retrieved successfully",
+            data: result.rows[0],
+        })
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
             error: error,
         })
     }
