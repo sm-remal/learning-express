6bd08f1e2fad222f81c6f991a6b52ac0d36ed4a9
sm-remal <remal.mozzem@gmail.com>
2026-06-03 14:17:34 +0600

    Added: Delete Method

--- src/server.ts
+++ src/server.ts
@@ -176,8 +176,40 @@ app.put('/api/users/:id', async (req: Request, res: Response) => {
             error: error,
         })
     }
-
 })
+
+
+
+// Delete user by id
+app.delete('/api/users/:id', async (req: Request, res: Response) => {
+    const {id} = req.params;
+
+    try {
+        const result = await pool.query(`
+            DELETE FROM users WHERE id = $1 RETURNING *
+            `,[id]);
+            if (result.rows.length === 0){
+                return res.status(404).json({
+                    success: false,
+                    message: "User not found",
+                })
+            }
+            
+            res.status(200).json({
+                success: true,
+                message: "User Deleted Successfully",
+                data: {},
+            })
+    } catch (error: any) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+            error: error,
+        })
+        
+    }
+})
+
    
 // App listen 
 app.listen(port, () => {
