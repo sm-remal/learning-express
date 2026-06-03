ace4d415f84a53b39397eb86c34e255d6715cd9f
sm-remal <remal.mozzem@gmail.com>
2026-06-03 11:03:28 +0600

    Create Table and Insert Data

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express, { type Application, type Request, type Response } from "express"
-import {Pool} from "pg"
-const app: Application = express() 
+import { Pool } from "pg"
+const app: Application = express()
 const port = 5000
 
 // Middleware
@@ -15,17 +15,33 @@ const pool = new Pool({
 // Database Table
 const initDB = async () => {
     try {
-        
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(20) NOT NULL,
+                email VARCHAR(50) UNIQUE NOT NULL,
+                password VARCHAR(20) NOT NULL,
+                is_active BOOLEAN DEFAULT true,
+                age INT,
+
+                created_at TIMESTAMP DEFAULT NOW(),
+                updated_at TIMESTAMP DEFAULT NOW()
+            )
+      `)
+
+        console.log("Table created successfully");
+
     } catch (error) {
         console.log(error);
     }
 }
 
+initDB();
 
 
 // Get request
 app.get('/', (req: Request, res: Response) => {
-//   res.send('Hello World!')
+    //   res.send('Hello World!')
     res.status(200).json({
         message: "Explore express with TypeScript",
         author: "Next Level"
@@ -35,16 +51,29 @@ app.get('/', (req: Request, res: Response) => {
 // post request
 app.post('/', async (req: Request, res: Response) => {
     // console.log(req.body)
-    const {name, email, password} = req.body;
-    res.status(201).json({
-        message: "Data received successfully",
-        data: {
-            name,
-            email,
-        }
-    })
+    const { name, email, password, age } = req.body;
+
+    try {
+        const result = await pool.query(`
+        INSERT INTO users (name, email, password, age)
+        VALUES ($1, $2, $3, $4) RETURNING *
+    `, [name, email, password, age]
+        )
+        // console.log(result.rows[0]);
+
+        res.status(201).json({
+            message: "User created successfully",
+            data: result.rows[0],
+        })
+    } catch (error: any) {
+        res.status(500).json({
+            message: error.message, 
+            error: error,
+        })
+    }
 })
 
+// App listen 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
