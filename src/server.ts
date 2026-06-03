7196d3af3988e3a1ef6a33dc0fb3deeaa0729bf0
sm-remal <remal.mozzem@gmail.com>
2026-06-03 14:04:58 +0600

    Update put method

--- src/server.ts
+++ src/server.ts
@@ -144,10 +144,10 @@ app.put('/api/users/:id', async (req: Request, res: Response) => {
     try {
         const result = await pool.query(`
             UPDATE users  
-                SET name = $1, 
-                password = $2, 
-                age = $3, 
-                is_active = $4
+                SET name = COALESCE($1, name), 
+                password = COALESCE($2, password), 
+                age = COALESCE($3, age), 
+                is_active = COALESCE($4, is_active)
             WHERE id = $5  
             RETURNING *
             `, [name, password, age, is_active, id]);
