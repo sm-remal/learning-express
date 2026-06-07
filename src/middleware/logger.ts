e7fa7a3cb6cdba1e28054ca0327aa212d0e644ad
sm-remal <remal.mozzem@gmail.com>
2026-06-07 21:18:28 +0600

    Added: middleware - logger.ts

---
+++ src/middleware/logger.ts
@@ -0,0 +1,15 @@
+import type { NextFunction, Request, Response } from "express";
+import fs from "fs";
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+    console.log("Method - URL - Time", req.method, req.url, Date.now());
+    const log = `\n Method -> ${req.method}, Time -> ${Date.now()}, Time -> ${req.url}}`
+
+    fs.appendFile("logger.ts", log, (error) => {
+        console.log(error);
+    })
+
+    next();
+}
+
+export default logger;
